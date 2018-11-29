e31e94efa39ae0ed2699c5c53f2fe65ee3515aca
simonvanbennekom <simonvanbennekom@gmail.com>
2018-11-29 14:31:50 +0000

    ProcessSLD, check for empty post body. Added file parameter to add a file name to the request

--- Server.py
+++ Server.py
@@ -1,10 +1,16 @@
 # File: Server.py
 
-from flask import Flask
-from flask import request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/processSLD', methods=['POST'])
-def proccessSLD():
-    return 'Hello, World!'
+@app.route('/processSLD/<file>', methods=['POST'])
+def processSLD(file) :
+    def catch_all(file):
+        request_data = request.get_data()
+        if not request_data :
+            return 'No SLD '
+        else :
+            app.logger.debug('Body: %s', request_data)
+            return 'File argument: %s' % file
+    return "hallo"

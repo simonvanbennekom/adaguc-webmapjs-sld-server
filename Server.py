62be5180497bbc3a29a58bceadf6f233b2ca4e33
simonvanbennekom <simonvanbennekom@gmail.com>
2018-12-05 13:58:06 +0000

    Returns url to XML, Generate a JSON response for client. Able to read XML files from the Flask server

--- Server.py
+++ Server.py
@@ -1,6 +1,6 @@
 # File: Server.py
 
-from flask import Flask, request
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 
 import configparser
@@ -16,11 +16,16 @@ CORS(app)
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+#Retrieve SLD files from sldFiles folder
+@app.route('/' + config.get('SLD', 'DIR') + '<path>', methods=['GET'])
+def send_js(path):
+    return send_from_directory(config.get('SLD', 'DIR'), path)
+
 @app.route('/processSLD', methods=['POST'])
 @app.route('/processSLD/<fileName>', methods=['POST'])
 def processSLD(fileName = '') :
     if not request.get_data() :
-        return 'SLD file cannot be empty', 400
+        return createErrorResponse('SLD file cannot be empty')
     else :
         if isValidFileSize(request.get_data()) :
             #read SLD data into string
@@ -28,19 +33,19 @@ def processSLD(fileName = '') :
             
             # starts with <?xml> and ends with StyledLayerDescriptor>
             if(requestData.startswith('<?xml') == False or requestData.endswith('StyledLayerDescriptor>') == False) :
-                return 'Invalid SLD, not matching standard', 400
+                return createErrorResponse('Invalid SLD, not matching standard')
                 
             #Parse XML and catch error if raised
             try:
                 tree = ET.ElementTree(ET.fromstring(requestData))
             except ET.ParseError as error:
                 row, column = error.position
-                return 'Invalid XML on line number: %i' % row, 400
-                        
+                return createErrorResponse('Invalid XML on line number: %i' % row)
+
             #Have FileName
             if fileName :
                 if fileNameExists(fileName) : 
-                    return 'Your file name:  %s.xml already exists. Try another' % fileName, 400
+                    return createErrorResponse('Your file name:  %s.xml already exists. Try another' % fileName)
             else :
                 #Generate a filename
 
@@ -56,21 +61,18 @@ def processSLD(fileName = '') :
             if fileName :
                 #Check if fileName contain at least 4 characters
                 if len(fileName) >= 4 :
-                    fileUploadPath = os.path.join(config.get('SLD', 'DIR'), fileName + '.xml')
+                    fileUploadPath = os.path.join(config.get('SLD', 'DIR'), getFileNameWithExtension(fileName))
                     file = open(fileUploadPath, "w")
                     file.write(requestData)
-
-                    #TODO return SLD URL
-                    url = os.path.realpath(file.name)
                     file.close()
 
-                    return url, 200
+                    return createSuccessResponse('SLD is succesfully created!', {'url' : request.host_url + config.get('SLD', 'DIR') + getFileNameWithExtension(fileName) })
                 else :
-                    return 'Filename must contain at least 4 characters', 400
+                    return createErrorResponse('Filename must contain at least 4 characters')
             else : 
-                return 'Something went wrong generating a fileName, try to submit again...', 400        
+                return createErrorResponse('Something went wrong generating a fileName, try to submit again...')
         else : 
-            return 'File size to big', 400
+            return createErrorResponse('File size to big')
 
 
 #Check if file size is not greater then the MaxFileSize
@@ -92,5 +94,17 @@ def fileNameExists(fileName) :
 
 #Get fileNames from dir where sldFiles are stored. 
 def getAllFileNames(filter = '*.xml') :
-    path = config.get('SLD', 'DIR') + filter
+    path = './' + config.get('SLD', 'DIR') + filter
     return [os.path.splitext(os.path.basename(x))[0] for x in glob.glob(path)]
+
+def createErrorResponse(message, data = None) :
+    return createResponse('error', message, data)
+
+def createSuccessResponse(message, data = None) :
+    return createResponse('success', message, data)    
+
+def createResponse(status,message, data = None) :
+    return jsonify(status=status,message=message,data=data)
+
+def getFileNameWithExtension(fileName) :
+    return fileName + '.xml'
